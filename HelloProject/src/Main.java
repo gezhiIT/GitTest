0dffc371caee0c930ba6a984f69a5bee073ec28c
Gezhi <wyh_56@qq.com>
2018-12-03 20:28:12 +0800

    user1修改

--- HelloProject/src/Main.java
+++ HelloProject/src/Main.java
@@ -5,5 +5,9 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("HelloGitHub!");
+        System.out.println("HelloJava!");
+        System.out.println("Hello123!");
+        System.out.println("Hello567!");
     }
 }
